a0dea8b231c85c149fb3e9ff25940d2703c6b113
abuUsman_04 <i123l183l@gmail.com>
2022-10-18 11:34:35 +0300

    Add project files.

---
+++ Al_Quran_Web_App/Controllers/RecitersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Al_Quran_Web_App.Controllers
+{
+    public class RecitersController : Controller
+    {
+        public IActionResult Index(string NameOfReciter)
+        {
+            if (NameOfReciter == "Mishary")
+            {
+                return RedirectToAction("MisharyReciter");
+            }
+            return View();
+        }
+
+        public IActionResult MisharyReciter()
+        {
+            return View();
+        }
+    }
+}
---
+++ Al_Quran_Web_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Al_Quran_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
